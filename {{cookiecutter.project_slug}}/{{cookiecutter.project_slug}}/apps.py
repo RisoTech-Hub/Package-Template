e896521855de21774285fdeabfdc372757bf8e1f
Bin Nguyễn <tu.nguyen@risotech.vn>
2023-08-22 11:55:03 +0700

    Fix generation

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class {{cookiecutter.project_slug|title}}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "{{cookiecutter.project_slug}}
+    name = "{{cookiecutter.project_slug}}"
